d9945ca13b5b115022d135861ea76a367e8bdd3e
Adolph Vogel <ajvogel@gmail.com>
2026-02-14 22:22:39 +0200

    Added another test on negBinom

--- tests/test_negbinom.py
+++ tests/test_negbinom.py
@@ -1,7 +1,12 @@
 import ranvar as rv
 
-def test_nonNeg():
+def test_nonNegLower():
     rv1 = rv.NegativeBinomial(p=0.002560779768182425, n=0.010000000783403946).compute()
     assert rv1.lower() >= 0
+
+
+def test_nonNegUpper():
+    rv1 = rv.NegativeBinomial(p=0.002560779768182425, n=0.010000000783403946).compute()
+    assert rv1.lower() <= 1000    
     
     
